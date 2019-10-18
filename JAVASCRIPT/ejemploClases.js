710a008b0cc180e7dab2c2ede513a431aa089865
Javijambo <javijambo98@gmail.com>
2019-10-18 20:53:28 +0200

    jordi

---
+++ JAVASCRIPT/ejemploClases.js
@@ -0,0 +1,13 @@
+class clase {
+    constructor(at1, at2) {
+        this.at1 = at1;
+        this.at2 = at2;
+    }
+
+    set name(valor) {
+        this.names_.push(valor);
+    }
+    set at1(valor) {
+        this.at1 = valor;
+    }
+}
