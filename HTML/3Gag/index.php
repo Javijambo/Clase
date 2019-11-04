dcf7bf8056d739d47b71ce0f2dd0f0019f990a28
javijambo <39520225+Javijambo@users.noreply.github.com>
2019-11-04 14:29:07 +0100

    Update index.php

--- HTML/3Gag/index.php
+++ HTML/3Gag/index.php
@@ -1,26 +1,3 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-    <title>Title</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-</head>
-
-<body>
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-</body>
-
-</html>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -32,77 +9,114 @@
     <link rel="stylesheet" href="istyle.css">
     <title>3GAG</title>
 </head>
+<h1 class="banner display-1 font-weight-bold card-title text-center">3GAG</h1>
+<body class="bg-dark">
+    <!-- Menu Izquierdo-->
 
-<body>
-    <ul>
-        <li><a class="active" href="#home">Inicio</a></li>
-        <li><a href="#news">News</a></li>
-        <li><a href="#contact">Contact</a></li>
-        <li><a href="#about">About</a></li>
-    </ul>
     <?php
-    $servername = "192.168.87.55";
-    $username = "3gag";
-    $password = "3gag";
-    $dbname = "3gag";
-    $conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
-    include_once("db_connect.php");
-    $sql = "SELECT id, name, image, description, address, website, facebook, gplus, twitter FROM cards";
-    $resultset = mysqli_query($conn, $sql) or die("database error:" . mysqli_error($conn));
-    while ($record = mysqli_fetch_assoc($resultset)) {
-        ?>
-        <div class="card hovercard">
-            <div class="cardheader">
-                <div class="avatar">
-                    <img alt="" src="<?php echo $record['imagen']; ?>">
-                </div>
+    //funcion para cambiar de categoria
+    function categoria($categoria)
+    {
+        $servername = "localhost";
+        $username = "3gag";
+        $password = "3gag";
+        $dbname = "3gag";
+        $conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection fai   led: " . mysqli_connect_error());
+        $sql = "SELECT id,titulo, imagen, categoria, usuario, mg FROM memes where categoria='" . $categoria . "';";
+        $resultset = mysqli_query($conn, $sql) or die("database error:" . mysqli_error($conn));
+        $todo = '';
+        while ($record = mysqli_fetch_assoc($resultset)) {
+            $imagen = $record['imagen'];
+            $titulo = $record['titulo'];
+            $mg = (int) $record['mg'];
+            $user = $record['usuario'];
+            $id = $record['id'];
+
+            $todo = $todo . '
+                <div class="card mt-5 ">
+                    <h1 class=" display-4 font-weight-bold card-title text-center">' . $titulo . '</h1>
+                    <img class="card-img-top " src="imagenes/' . $imagen . '" alt="Card image cap">
+                    <p class="texto card-text mt-3 text-white ">Usuario:' . $user . '
+                        Likes:' . $mg . '
+                        <button class="btn btn-dark text-white mr-5 float-right mt-2" onclick="mg(' . $id . ',' . $mg . ')">Me gusta</button>
+                        <button class="btn btn-dark text-white text-left float-right mr-5 mb-3 mt-2" onclick="nomg(' . $id . ',' . $mg . ')" > No me gusta</button></p>
+                </div>';
+        }
+        echo '<script type="text/javascript">location.reload(true);</script>';
+
+        return $todo;
+    }
+    //boton dar me gusta
+    function mg($id, $mg)
+    {
+        $mg2 = $mg + 1;
+        $servername = "localhost";
+        $username = "3gag";
+        $password = "3gag";
+        $dbname = "3gag";
+        $conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
+        $sql = "Update memes set mg=" . $mg2 . " where id='" . $id . "';";
+        echo '<script type="text/javascript">location.reload(true);</script>';
+        return mysqli_query($conn, $sql) or die("database error:" . mysqli_error($conn));
+    }
+    //boton no me gusta
+    function nomg($id, $mg)
+    {
+        $mg2 = $mg - 1;
+        $servername = "localhost";
+        $username = "3gag";
+        $password = "3gag";
+        $dbname = "3gag";
+        $conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
+        $sql = "Update memes set mg=" . $mg2 . " where id='" . $id . "';";
+        echo '<script type="text/javascript">location.reload(true);</script>';
+        return mysqli_query($conn, $sql) or die("database error:" . mysqli_error($conn));
+    }
+    ?>
+    <ul>
+        <li><a class="active" href="index.php">Inicio</a></li>
+        <li><a href="subirmeme.php">Subir meme</a></li>
+        <li><a href="login.php">Login</a></li>
+        <li><a href="registro.php">Registrarse</a></li>
+    </ul>
+    <div class="sidenav">
+        <a href="#">Spain memes</a>
+        <a href="#">Funny memes</a>
+        <a href="#">Animals memes</a>
+        <a href="#">Anime & Manga memes</a>
+        <a href="#">Comic memes</a>
+        <a href="#">Car memes</a>
+        <a href="#">Cosplay memes</a>
+    </div>
+
+    <div class="todo bg-dark">
+        <?php
+        $servername = "localhost";
+        $username = "3gag";
+        $password = "3gag";
+        $dbname = "3gag";
+        $conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
+
+        $sql = "SELECT id,titulo, imagen, categoria, usuario, mg FROM memes";
+        $resultset = mysqli_query($conn, $sql) or die("database error:" . mysqli_error($conn));
+
+        while ($record = mysqli_fetch_assoc($resultset)) {
+            $imagen = $record['imagen'];
+            $titulo = $record['titulo'];
+            $mg = $record['mg'];
+            $user = $record['usuario'];
+            $id = $record['id'];
+            ?>
+            <div class="card mt-5 ">
+                <h1 class=" display-4 font-weight-bold card-title text-center"><?php echo $titulo; ?></h1>
+                <img class="card-img-top " src='imagenes/<?php echo $imagen; ?>' alt="Card image cap">
+                <p class="texto card-text mt-3 text-white ">Usuario: <?php echo $user ?>
+                    Likes:<?php echo $mg; ?>
+                    <button class="btn btn-dark text-white mr-5 float-right mt-2" onclick="mg(<?php $id; ?>,<?php $mg; ?>)">Me gusta</button>
+                    <button class="btn btn-dark text-white text-left float-right mr-5 mb-3 mt-2" onclick="nomg(<?php $id; ?>,<?php $mg; ?>)"> No me gusta</button></p>
             </div>
-            <div class="card-body info">
-                <div class="title">
-                    <a href="#"><?php echo $record['titulo']; ?></a>
-                </div>
-                <div class="desc"> <label> <?php echo $record['usuario']; ?></label></div>
-            </div>
-            <div class="card-footer bottom">
-                <button class="megusta" onclick="simg($record['id']);">Me Gusta</button>
-                <label><?php echo $record['mg']; ?></label>
-                <button class="nogusta" onclick="nomg($record['id']);">No me Gusta</button>
-            </div>
-        </div>
-    <?php } ?>
 
-
-    <div class="todo">
-        Both rest of know draw fond post as. It agreement defective to excellent. Feebly do engage of narrow. Extensive repulsive belonging depending if promotion be zealously as. Preference inquietude ask now are dispatched led appearance. Small meant in so
-        doubt hopes. Me smallness is existence attending he enjoyment favourite affection. Delivered is to ye belonging enjoyment preferred. Astonished and acceptance men two discretion. Law education recommend did objection how old.
-
-        <br> say express had chiefly detract demands she. Quiet led own cause three him. Front no party young abode state up. Saved he do fruit woody of to. Met defective are allowance two perceived listening consulted contained. It chicken oh colonel
-        pressed excited suppose to shortly. He improve started no we manners however effects. Prospect humoured mistress to by proposal marianne attended. Simplicity the far admiration preference everything. Up help home head spot an he room in.
-
-        <br> Doubtful two bed way pleasure confined followed. Shew up ye away no eyes life or were this. Perfectly did suspicion daughters but his intention. Started on society an brought it explain. Position two saw greatest stronger old. Pianoforte
-        if at simplicity do estimating.
-
-        <br> Certainly elsewhere my do allowance at. The address farther six hearted hundred towards husband. Are securing off occasion remember daughter replying. Held that feel his see own yet. Strangers ye to he sometimes propriety in. She right plate
-        seven has. Bed who perceive judgment did marianne.
-
-        <br> She who arrival end how fertile enabled. Brother she add yet see minuter natural smiling article painted. Themselves at dispatched interested insensible am be prosperous reasonably it. In either so spring wished. Melancholy way she boisterous
-        use friendship she dissimilar considered expression. Sex quick arose mrs lived. Mr things do plenty others an vanity myself waited to. Always parish tastes at as mr father dining at.
-
-        <br> Death there mirth way the noisy merit. Piqued shy spring nor six though mutual living ask extent. Replying of dashwood advanced ladyship smallest disposal or. Attempt offices own improve now see. Called person are around county talked her
-        esteem. Those fully these way nay thing seems.
-
-        <br> We diminution preference thoroughly if. Joy deal pain view much her time. Led young gay would now state. Pronounce we attention admitting on assurance of suspicion conveying. That his west quit had met till. Of advantage he attending household
-        at do perceived. Middleton in objection discovery as agreeable. Edward thrown dining so he my around to.
-
-        <br> In as name to here them deny wise this. As rapid woody my he me which. Men but they fail shew just wish next put. Led all visitor musical calling nor her. Within coming figure sex things are. Pretended concluded did repulsive education smallness
-        yet yet described. Had country man his pressed shewing. No gate dare rose he. Eyes year if miss he as upon.
-
-        <br> Attended no do thoughts me on dissuade scarcely. Own are pretty spring suffer old denote his. By proposal speedily mr striking am. But attention sex questions applauded how happiness. To travelling occasional at oh sympathize prosperous.
-        His merit end means widow songs linen known. Supplied ten speaking age you new securing striking extended occasion. Sang put paid away joy into six her.
-
-        <br> Exquisite cordially mr happiness of neglected distrusts. Boisterous impossible unaffected he me everything. Is fine loud deal an rent open give. Find upon and sent spot song son eyes. Do endeavor he differed carriage is learning my graceful.
-        Feel plan know is he like on pure. See burst found sir met think hopes are marry among. Delightful remarkably new assistance saw literature mrs favourable.
-
+        <?php } ?>
     </div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
