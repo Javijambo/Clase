05d90b3d1eb4b2c060a2ac8af28bf110f754ea51
Javijambo <javijambo98@gmail.com>
2020-02-04 19:45:18 +0100

    terminado entre comillas

--- JAVASCRIPT/snake/src/App.js
+++ JAVASCRIPT/snake/src/App.js
@@ -1,33 +1,29 @@
 import React, { Component } from 'react';
-import Snake from './Snake';
-import Food from './food';
+import Serpiente from './Serpiente';
+import Fruta from './Fruta';
 
 const coordenadas = () => {
-	let min = 1;
-	let max = 98;
-	let x = Math.floor((Math.random() * (max - min + 1) + min) / 2) * 2;
-	let y = Math.floor((Math.random() * (max - min + 1) + min) / 2) * 2;
-	return [x, y]
-}
+	let min = 5;
+	let max = 100;
+	let x = Math.floor((Math.random() * (max - min) + min) / 5) * 5;
+	let y = Math.floor((Math.random() * (max - min) + min) / 5) * 5;
+	return [ x, y ];
+};
 
-const initialState = {
-	speed: 300,
-	food: coordenadas(),
+const Inicio = {
+	velocidad: 300,
+	fruta: coordenadas(),
 	direction: 'RIGHT',
-	snakeDots: [
-		[0, 0],
-		[2, 0]
-	]
-}
+	cuerpoSerpiente: [ [ 0, 0 ], [ 5, 0 ] ]
+};
 
 class App extends Component {
-	state = initialState;
+	state = Inicio;
 
 	componentDidMount() {
-		setInterval(this.moversnake, this.state.speed);//seteamos un intervalo para el movimiento de la serpiente
+		setInterval(this.moversnake, this.state.velocidad); //seteamos un intervalo para el movimiento de la serpiente
 		document.onkeydown = this.tecla; //recogemos la tecla presionada
 	}
-
 
 	componentDidUpdate() {
 		this.salirsedelarea();
@@ -35,104 +31,113 @@ class App extends Component {
 		this.comer();
 	}
 
-
 	tecla = (e) => {
 		e = e || window.event; //cogemos la tecla que pulsamos comprobamos que no presionemos al sentido contrario para no perder automaticamente cuando damos a la flecha contraria
-		if (e.keyCode == 38  && this.state.direction!='DOWN') { 
-			this.setState({ direction: 'UP' });//flecha arriba
+//flecha arriba
+		if (e.keyCode === 38 && this.state.direction !== 'DOWN') {
+			this.setState({ direction: 'UP' }); 
+//flecha abajo
+		} else if (e.keyCode === 40 && this.state.direction !== 'UP') {
+			this.setState({ direction: 'DOWN' }); 
+//flecha izquierda
+		} else if (e.keyCode === 37 && this.state.direction !== 'RIGHT') {
+			this.setState({ direction: 'LEFT' }); 
+//flecha derecha
+		} else if (e.keyCode === 39 && this.state.direction !== 'LEFT') {
+			this.setState({ direction: 'RIGHT' }); 
 		}
-		else if (e.keyCode == 40  && this.state.direction!='UP') {
-			this.setState({ direction: 'DOWN' });//flecha abajo
-		}
-		else if (e.keyCode == 37 && this.state.direction!='RIGHT') {
-			this.setState({ direction: 'LEFT' });//flecha izquierda
-		}
-		else if (e.keyCode == 39 && this.state.direction!='LEFT') {
-			this.setState({ direction: 'RIGHT' });//flecha derecha
-		}
-	}
+	};
 
 	moversnake = () => {
-		let dots = [...this.state.snakeDots]; //guardar los puntitos del cuerpo de la serpiente
-		let head = dots[dots.length - 1]; //cabeza de la serpiente (largura de la serpiente -1 que equivale al último elemento)
-		//switch de sleccion de movimiento segun la dirección, el primer array de head contiene el valor x y el segundo el valor de y
+//guardar los puntitos del cuerpo de la serpiente
+		let bloques = [ ...this.state.cuerpoSerpiente ]; 
+//cabeza de la serpiente (array length -1 que equivale al último elemento)
+		let cabeza = bloques[bloques.length - 1]; 
+//switch de sleccion de movimiento segun la dirección, el primer array de cabeza contiene el valor x y el segundo el valor de y
 		switch (this.state.direction) {
 			case 'RIGHT':
-				head = [head[0] + 2, head[1]];//para ir a la derecha aumentamos x
+				cabeza = [ cabeza[0] + 5, cabeza[1] ]; //para ir a la derecha aumentamos x
 				break;
+
 			case 'LEFT':
-				head = [head[0] - 2, head[1]];//para ir a la izquierda disminuimos x
+				cabeza = [ cabeza[0] - 5, cabeza[1] ]; //para ir a la izquierda disminuimos x
 				break;
+
 			case 'DOWN':
-				head = [head[0], head[1] + 2];//para ir abajo aumentamos y
+				cabeza = [ cabeza[0], cabeza[1] + 5 ]; //para ir abajo aumentamos y
 				break;
+
 			case 'UP':
-				head = [head[0], head[1] - 2];//para ir arriba disminuimos y
+				cabeza = [ cabeza[0], cabeza[1] - 5 ]; //para ir arriba disminuimos y
+				break;
+
+			default:
 				break;
 		}
-		dots.push(head); //metemos el nuevo valor en el array para la cabeza para indicar el movimiento
-		dots.shift(); //eliminamos el primer elemento del array (la cola de la serpiente)
+		bloques.push(cabeza); //metemos el nuevo valor en el array para la cabeza para indicar el movimiento
+		bloques.shift(); //eliminamos el primer elemento del array (la cola de la serpiente)
 		this.setState({
-			snakeDots: dots
-		})
-	}
+			cuerpoSerpiente: bloques
+		});
+	};
 
 	//funcion de perder, alert y reset
 	gameover() {
-		alert("Game Over");
-		this.setState(initialState);
+		alert('Game Over');
+		this.setState(Inicio);
 	}
 
 	//funcion para comprobar si se sale del area la serpiente
 	salirsedelarea() {
-		let head = this.state.snakeDots[this.state.snakeDots.length - 1];//almacenamos en una variable la cabeza de la serpiente
-		if (head[0] >= 100 || head[1] >= 100 || head[0] < 0 || head[1] < 0) {
+		let cabeza = this.state.cuerpoSerpiente[this.state.cuerpoSerpiente.length - 1]; //almacenamos en una variable la cabeza de la serpiente
+		if (cabeza[0] >= 100 || cabeza[1] >= 100 || cabeza[0] < 0 || cabeza[1] < 0) {
 			this.gameover();
 		}
 	}
 
 	//funcion que comprueba si la serpiente se ha golpeado a si misma
 	serpientegolpeada() {
-		let snake = [ ...this.state.snakeDots ];
-		let head = snake[snake.length - 1];
+		let snake = [ ...this.state.cuerpoSerpiente ];
+		let cabeza = snake[snake.length - 1];
 		snake.pop();
-		snake.forEach(dot => {//si la serpiente se golpea a si misma
-			if (head[0] == dot[0] && head[1] == dot[1]) {
+		snake.forEach((punto) => {
+		//comprobamos si la serpiente se golpea a si misma en cada uno de sus puntos
+			if (cabeza[0] === punto[0] && cabeza[1] === punto[1]) {
 				this.gameover();
 			}
-		})
+		});
 	}
 	//funcion de comer
 	comer() {
-		let head = this.state.snakeDots[this.state.snakeDots.length - 1];
-		let food = this.state.food;
-		if (head[0] == food[0] && head[1] == food[1]) {
-			this.setState({//actualizamos la posicion de la fruta antes de aumentar el tamaño de la serpiente, si no se crea un bucle infinito
-				food:coordenadas()
-			})
+		let cabeza = this.state.cuerpoSerpiente[this.state.cuerpoSerpiente.length - 1];
+		let fruta = this.state.fruta;
+		if (cabeza[0] == fruta[0] && cabeza[1] == fruta[1]) {
+			this.setState({
+				//actualizamos la posicion de la fruta antes de aumentar el tamaño de la serpiente, si no se crea un bucle infinito
+				fruta: coordenadas()
+			});
 			this.crecer();
 			this.aumentarvelocidad();
 		}
 	}
-	crecer(){
-		let newSnake=[...this.state.snakeDots];
-		newSnake.unshift([])
+	crecer() {
+		let newSerpiente = [ ...this.state.cuerpoSerpiente ];
+		newSerpiente.unshift([]);
 		this.setState({
-			snakeDots:newSnake
-		})
+			cuerpoSerpiente: newSerpiente
+		});
 	}
-	aumentarvelocidad(){
-		if(this.state.speed>10){
+	aumentarvelocidad() {
+		if (this.state.velocidad > 10) {
 			this.setState({
-				speed: this.state.speed -10
-			})
+				velocidad: this.state.velocidad - 10
+			});
 		}
 	}
 	render() {
 		return (
-			<div className="game-area">
-				<Snake snakeDots={this.state.snakeDots} />
-				<Food dot={this.state.food} />
+			<div className="area">
+				<Serpiente cuerpoSerpiente={this.state.cuerpoSerpiente} /> <Fruta bloque={this.state.fruta} />{' '}
 			</div>
 		);
 	}
